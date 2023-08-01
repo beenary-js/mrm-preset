6e6dd59c4c59362d2e2b5a43125b4578aed4e95e
Beata Obrok <beata.obrok@u2i.com>
2023-08-01 18:58:52 +0200

    feat: add eslint task

---
+++ eslint/index.js
@@ -0,0 +1,41 @@
+const eslint = require("mrm-task-eslint");
+const { json, packageJson, install } = require("mrm-core");
+
+const hasPackage = (package, name) =>
+  package.get(`devDependencies.${name}`) || package.get(`dependencies.${name}`);
+
+const preset = (pkg, name) => {
+  const isReact = hasPackage(pkg, "react");
+  const preset = `${name}${isReact ? "" : "-base"}`;
+  const package = `eslint-config-${preset}`;
+  return { preset, package };
+};
+
+module.exports = function task(...args) {
+  eslint(...args);
+  const eslintrc = json(".eslintrc.json");
+  const presets = eslintrc.get("extends");
+  const pkg = packageJson();
+
+  const base = preset(pkg, "beenary");
+  const packages = [base.package];
+  presets.push(base.preset);
+
+  if (hasPackage(pkg, "typescript")) {
+    const typescript = preset(pkg, "beenary-typescript");
+    packages.push(typescript.package);
+    presets.push(typescript.preset);
+  }
+  if (hasPackage(pkg, "jest")) {
+    const jest = preset(pkg, "beenary-jest");
+    packages.push(jest.package);
+    presets.push(jest.preset);
+  }
+
+  eslintrc.set("extends", presets);
+  eslintrc.save();
+  install(packages);
+};
+
+module.exports.description = eslint.description;
+module.exports.parameters = eslint.parameters;
